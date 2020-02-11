56890c551d102e9584c6b3ae76bbd689c7284b00
Kayro Figueroa <kayrofigueroa@gmail.com>
2020-02-11 14:18:25 -0700

    Test3

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './../src/components/NavBar/Navbar'
-import Search from './../src/components/Search/Search'
-import Jumbotron from './../src/components/Jumbotron/Jumbotron'
+import Navbar from './components/NavBar/Navbar'
+import Search from './components/Search/Search'
+import Jumbotron from './components/Jumbotron/Jumbotron'
 class App extends Component {
 
   render() {
