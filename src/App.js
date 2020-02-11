ac0aeea7676436163dab2e59c3f9751cc2cdd485
Kayro Figueroa <kayrofigueroa@gmail.com>
2020-02-11 13:47:19 -0700

    Last test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './../src/components/NavBar/Navbar'
 import Search from './../src/components/Search/Search'
 import Jumbotron from './../src/components/Jumbotron/Jumbotron'
